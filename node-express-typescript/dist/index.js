32a340e587b2730d94fbc05bff5310d64aec030c
chryzcode <alabaolanrewaju13@gmail.com>
2024-03-09 19:40:14 +0100

    push

--- node-express-typescript/dist/index.js
+++ node-express-typescript/dist/index.js
@@ -14,12 +14,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const app_1 = __importDefault(require("./routes/app"));
 //For env File
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8000;
+app.use(app_1.default);
+app.use(express_1.default.json());
 app.get("/", (req, res) => {
-    res.send("Welcome to Express & TypeScript Server");
+    res.send("A TS app");
 });
 const start = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
--- node-express-typescript/src/routes/app.ts
+++ node-express-typescript/src/routes/app.ts
@@ -1,10 +1,10 @@
 import express, { Router } from "express";
-import {test} from "../controllers/app";
+import {test, testApi} from "../controllers/app";
 
 
 const router: Router = express.Router();
 
-router.route("/test").get(test);
+router.route("/test").get(test).post(testApi);
 
 
 export default router;
