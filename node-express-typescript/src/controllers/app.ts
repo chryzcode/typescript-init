bfef8900c0f5b8b2bdf072659e937f407286adc4
chryzcode <alabaolanrewaju13@gmail.com>
2024-03-09 19:12:59 +0100

    push

--- node-express-typescript/src/controllers/app.ts
+++ node-express-typescript/src/controllers/app.ts
@@ -1,4 +1,13 @@
 import { StatusCodes } from "http-status-codes";
+import dotenv from "dotenv";
+import OpenAI from "openai";
+
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+e
 
 export const test = async(req: Request, res: any) => {
     res.status(StatusCodes.OK).json({'hi':'hello'});
