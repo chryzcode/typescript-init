026154892114888d7a270eee80c1b2f73ac09a96
chryzcode <alabaolanrewaju13@gmail.com>
2024-03-09 19:38:27 +0100

    push

--- node-express-typescript/src/controllers/app.ts
+++ node-express-typescript/src/controllers/app.ts
@@ -2,13 +2,25 @@ import { StatusCodes } from "http-status-codes";
 import dotenv from "dotenv";
 import OpenAI from "openai";
 
+dotenv.config();
 
-const openai = new OpenAI({
+const openai: OpenAI = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-e
+export const testApi = async (req: any, res: any) => {
+  const { message } = req.body;
+  if (!message) {
+    throw new Error("Message input is required");
+  }
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: message }],
+    model: "gpt-3.5-turbo",
+  });
+  console.log(completion.choices[0]);
+  res.status(StatusCodes.OK).json({ completion });
+};
 
-export const test = async(req: Request, res: any) => {
-    res.status(StatusCodes.OK).json({'hi':'hello'});
+export const test = async (req: Request, res: any) => {
+  res.status(StatusCodes.OK).json({ hi: "hello" });
 };
